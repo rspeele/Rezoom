a4be403e0c33d93c0969a8b3f0d16fb845b0f470
Robert Peele <robert.s.peele@gmail.com>
2016-06-10 18:07:26 -0400

    Add first tests for dataseqs.

---
+++ Data.Resumption.Test/TestDataSeq.fs
@@ -0,0 +1,60 @@
+﻿namespace Data.Resumption.Test
+open Data.Resumption
+open System
+open System.Text
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestDataSeq() =
+    [<TestMethod>]
+    member __.TestSimpleIteration() =
+        let sequence =
+            dataseq {
+                yield "x"
+                yield "y"
+                yield "z"
+            }
+        {
+            Task = fun () ->
+                datatask {
+                    for query in sequence do
+                        let! q = send query
+                        printfn "%s" q
+                    return () // we have to have this return because for over dataseqs is strict
+                }
+            Batches =
+                [
+                    ["x"]
+                    ["y"]
+                    ["z"]
+                ]
+            Result = Good ()
+        } |> test
+
+    [<TestMethod>]
+    member __.TestQueryIteration() =
+        let sequence =
+            dataseq {
+                let! x = send "x"
+                yield x
+                let! y = send "y"
+                yield y
+                let! z = send "z"
+                yield z
+            }
+        {
+            Task = fun () ->
+                datatask {
+                    let builder = new StringBuilder()
+                    for result in sequence do
+                        ignore <| builder.Append(result)
+                    return builder.ToString()
+                }
+            Batches =
+                [
+                    ["x"]
+                    ["y"]
+                    ["z"]
+                ]
+            Result = Good "xyz"
+        } |> test
