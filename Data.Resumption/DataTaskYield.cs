1d5ee4069af92f1f037faef3068f23db044985bd
Robert Peele <robert.s.peele@gmail.com>
2016-06-11 14:39:13 -0400

    Throw exception on accessing .Value when .HasValue is false.

--- Data.Resumption/DataTaskYield.cs
+++ Data.Resumption/DataTaskYield.cs
@@ -1,16 +1,26 @@
-﻿namespace Data.Resumption
+﻿using System;
+
+namespace Data.Resumption
 {
     public struct DataTaskYield<T>
     {
+        private readonly T _value;
         public DataTaskYield(T value)
         {
-            Value = value;
+            _value = value;
             HasValue = true;
         }
         public bool HasValue { get; }
         /// <summary>
         /// The value yielded from this iteration.
         /// </summary>
-        public T Value { get; }
+        public T Value
+        {
+            get
+            {
+                if (!HasValue) throw new InvalidOperationException("Cannot obtain a value from the end of a sequence");
+                return Value;
+            }
+        }
     }
 }
