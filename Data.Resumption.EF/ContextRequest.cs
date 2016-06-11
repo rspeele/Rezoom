bcd14a2113bf4dffe49dc67034cd6ac57610bebd
Robert Peele <robert.s.peele@gmail.com>
2016-06-11 19:32:42 -0400

    Add Data.Resumption.EF with some base request classes for batching CRUD ops.

---
+++ Data.Resumption.EF/ContextRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Data.Resumption.DataRequests;
+using Data.Resumption.Services;
+
+namespace Data.Resumption.EF
+{
+    public abstract class ContextRequest<TContext, T> : DataRequest<T>
+        where TContext : DbContext
+    {
+        public override object DataSource => typeof(TContext);
+        public override object SequenceGroup => typeof(TContext);
+
+        protected abstract Func<Task<T>> Prepare(TContext db);
+
+        public sealed override Func<Task<T>> Prepare(IServiceContext context)
+        {
+            var db = context.GetService<TContext>();
+            return Prepare(db);
+        }
+    }
+}
