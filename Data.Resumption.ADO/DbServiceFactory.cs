d03da19a0e71c1812da2b8d4b9d22d3c8b4387b3
Robert Peele <robert.s.peele@gmail.com>
2016-06-11 17:29:04 -0400

    Add DbServiceFactory and DbTypeRecognizer.

---
+++ Data.Resumption.ADO/DbServiceFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Common;
+using Data.Resumption.Services;
+
+namespace Data.Resumption.ADO
+{
+    public abstract class DbServiceFactory : IServiceFactory
+    {
+        protected abstract DbConnection CreateConnection();
+        protected virtual IDbTypeRecognizer CreateDbTypeRecognizer() => new DbTypeRecognizer();
+
+        public LivingService<T>? CreateService<T>(IServiceContext context)
+        {
+            if (typeof(T) == typeof(DbConnection))
+            {
+                var conn = CreateConnection();
+                return new LivingService<T>((T)(object)conn, ServiceLifetime.ExecutionContext);
+            }
+            if (typeof(T) == typeof(IDbTypeRecognizer))
+            {
+                var recognizer = CreateDbTypeRecognizer();
+                return new LivingService<T>((T)recognizer, ServiceLifetime.ExecutionContext);
+            }
+            if (typeof(T) == typeof(CommandContext))
+            {
+                var dbConnection = context.GetService<DbConnection>();
+                var dbTypeRecognizer = context.GetService<IDbTypeRecognizer>();
+                var cmdContext = new CommandContext(dbConnection, dbTypeRecognizer);
+                return new LivingService<T>((T)(object)cmdContext, ServiceLifetime.ExecutionStep);
+            }
+            return null;
+        }
+    }
+}
