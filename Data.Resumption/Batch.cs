d5bc0a2ac4574be5d522cd968b368c35c0ee8dab
Robert Peele <robert.s.peele@gmail.com>
2016-06-25 19:39:18 -0400

    Yet more XML comments.

--- Data.Resumption/Batch.cs
+++ Data.Resumption/Batch.cs
@@ -6,11 +6,22 @@ namespace Data.Resumption
 {
     /// <summary>
     /// An immutable tree of Ts.
-    /// Used to represent a batch of data requests, and their eventual responses in the same tree shape.
+    /// Used to represent a batch of data requests.
     /// </summary>
+    /// <remarks>
+    /// A tree structure is used because it allows a pair of batches to be combined into one in O(1) time
+    /// without undue copying. It also can be easily mapped over to get a matching tree shape to correlate responses.
+    /// </remarks>
     /// <typeparam name="T"></typeparam>
     public abstract class Batch<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Map a function over this batch to get another batch with the mapped elements
+        /// arranged in the same tree structure.
+        /// </summary>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="mapping"></param>
+        /// <returns></returns>
         public abstract Batch<TOut> Map<TOut>(Func<T, TOut> mapping);
         internal abstract BatchLeaf<T> AssumeLeaf();
         internal abstract BatchBranchN<T> AssumeBranchN();
--- Data.Resumption/RequestsPending.cs
+++ Data.Resumption/RequestsPending.cs
@@ -2,6 +2,11 @@
 
 namespace Data.Resumption
 {
+    /// <summary>
+    /// Represents a batch of pending <see cref="IDataRequest"/>s paired with a function to resume execution with an
+    /// <see cref="IDataTask{TResult}"/> based on the results of those requests.
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
     public struct RequestsPending<TResult>
     {
         public RequestsPending
@@ -13,9 +18,26 @@ namespace Data.Resumption
             Resume = resume;
         }
 
+        /// <summary>
+        /// The batch of <see cref="IDataRequest"/>s that the task is waiting on.
+        /// </summary>
         public Batch<IDataRequest> Requests { get; }
+        /// <summary>
+        /// A function to get the continuation <see cref="IDataTask{TResult}"/> based on
+        /// a batch of <see cref="SuccessOrException"/>s representing the results of <see cref="Requests"/>.
+        /// </summary>
         public Func<Batch<SuccessOrException>, IDataTask<TResult>> Resume { get; }
 
+        /// <summary>
+        /// Transform the continuation of this pending state.
+        /// </summary>
+        /// <remarks>
+        /// The resulting <see cref="RequestsPending{TOutResult}"/> will have the same batch of requests,
+        /// but its <see cref="Resume"/> continuation will be transformed by <paramref name="mapping"/>.
+        /// </remarks>
+        /// <typeparam name="TOutResult"></typeparam>
+        /// <param name="mapping"></param>
+        /// <returns></returns>
         public RequestsPending<TOutResult> Map<TOutResult>
             (Func<IDataTask<TResult>, IDataTask<TOutResult>> mapping)
         {
