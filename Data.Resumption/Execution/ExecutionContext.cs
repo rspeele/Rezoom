4bf846221cfc778a2017d27111ec4c4168c5bcd0
Robert Peele <robert.s.peele@gmail.com>
2016-07-02 15:09:05 -0400

    Add XML comments for ExecutionContext.

--- Data.Resumption/Execution/ExecutionContext.cs
+++ Data.Resumption/Execution/ExecutionContext.cs
@@ -3,12 +3,22 @@ using Data.Resumption.Services;
 
 namespace Data.Resumption.Execution
 {
+    /// <summary>
+    /// Handles execution of an <see cref="IDataTask{TResult}"/> by stepping through it and running its pending
+    /// <see cref="IDataRequest"/>s with caching and deduplication.
+    /// </summary>
     public class ExecutionContext : IServiceContext
     {
         private readonly IExecutionLog _log;
         private readonly ServiceContext _serviceContext;
         private readonly ResponseCache _responseCache = new ResponseCache();
 
+        /// <summary>
+        /// Create an execution context by giving it an <see cref="IServiceFactory"/> to provide
+        /// services required by the <see cref="IDataRequest"/>s that it'll be responsible for executing.
+        /// </summary>
+        /// <param name="serviceFactory"></param>
+        /// <param name="log"></param>
         public ExecutionContext(IServiceFactory serviceFactory, IExecutionLog log = null)
         {
             _serviceContext = new ServiceContext(serviceFactory);
@@ -36,6 +46,12 @@ namespace Data.Resumption.Execution
             return pending.Resume(responses);
         }
 
+        /// <summary>
+        /// Asynchronously run the given <see cref="IDataTask{TResult}"/> to completion.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <returns></returns>
         public async Task<T> Execute<T>(IDataTask<T> task)
         {
             var executing = true;
