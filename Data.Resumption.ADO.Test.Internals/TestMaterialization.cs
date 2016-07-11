2bb9283cb29c9340ab44c06b7dd677ad3afbec6a
Robert Peele <robert.s.peele@gmail.com>
2016-07-10 20:35:08 -0400

    Add test for nested arrays.

--- Data.Resumption.ADO.Test.Internals/TestMaterialization.cs
+++ Data.Resumption.ADO.Test.Internals/TestMaterialization.cs
@@ -52,5 +52,51 @@ namespace Data.Resumption.ADO.Test.Internals
             Assert.AreEqual(3, user.Groups[1].Id);
             Assert.AreEqual("testers", user.Groups[1].Name);
         }
+
+        public class NestUser
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Group[] Groups { get; set; }
+
+            public class Group
+            {
+                public int Id { get; set; }
+                public string Name { get; set; }
+                public Tag[] Tags { get; set; }
+            }
+            public class Tag
+            {
+                public int Id { get; set; }
+                public string Name { get; set; }
+            }
+        }
+
+        [TestMethod]
+        public void TestNestedArrayNavProperty()
+        {
+            var template = RowReaderTemplate<NestUser>.Template;
+            var reader = template.CreateReader();
+            var columnMap = ColumnMap.Parse(new[] { "Id", "Name", "Groups$Id", "Name", "Groups$Tags$Id", "Name" });
+            reader.ProcessColumnMap(columnMap);
+            reader.ProcessRow(new object[] { 1, "bob", 2, "developers", 4, "t1" });
+            reader.ProcessRow(new object[] { 1, "bob", 2, "developers", 5, "t2" });
+            reader.ProcessRow(new object[] { 1, "bob", 2, "developers", 6, "t3" });
+            reader.ProcessRow(new object[] { 1, "bob", 3, "testers", 4, "t1" });
+            reader.ProcessRow(new object[] { 1, "bob", 3, "testers", 5, "t2" });
+            reader.ProcessRow(new object[] { 1, "bob", 3, "testers", 7, "t4" });
+            var user = reader.ToEntity();
+            Assert.AreEqual(1, user.Id);
+            Assert.AreEqual("bob", user.Name);
+            Assert.AreEqual(2, user.Groups.Length);
+            Assert.AreEqual(2, user.Groups[0].Id);
+            Assert.AreEqual("developers", user.Groups[0].Name);
+            Assert.AreEqual(3, user.Groups[0].Tags.Length);
+            Assert.AreEqual("t2", user.Groups[0].Tags[1].Name);
+            Assert.AreEqual(3, user.Groups[1].Id);
+            Assert.AreEqual("testers", user.Groups[1].Name);
+            Assert.AreEqual(3, user.Groups[1].Tags.Length);
+            Assert.AreEqual("t4", user.Groups[1].Tags[2].Name);
+        }
     }
 }
