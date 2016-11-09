f154c302164b976b466dbf53214399437c4b70de
Robert Peele <robert.s.peele@gmail.com>
2016-11-09 00:31:46 -0500

    Switch to FsUnit for Rezoom.Test unit tests.

--- Rezoom.Test/TestCaching.fs
+++ Rezoom.Test/TestCaching.fs
@@ -1,119 +1,118 @@
-﻿namespace Rezoom.Test
+﻿module Rezoom.Test.TestCaching
 open Rezoom
-open Microsoft.VisualStudio.TestTools.UnitTesting
+open NUnit.Framework
+open FsUnit
 
-[<TestClass>]
-type TestCaching() =
-    [<TestMethod>]
-    member __.TestStrictCachedPair() =
-        {   Task = fun () ->
-                plan {
-                    let! q1 = send "q"
-                    let! q2 = send "q"
-                    return q1 + q2
-                }
-            Batches =
-                [   [ "q" ]
-                ]
-            Result = Good "qq"
-        } |> test
-        
-    [<TestMethod>]
-    member __.TestConcurrentCachedPair() =
-        {   Task = fun () ->
-                plan {
-                    let! q1, q2 = send "q", send "q"
-                    return q1 + q2
-                }
-            Batches =
-                [   [ "q" ]
-                ]
-            Result = Good "qq"
-        } |> test
-
-    [<TestMethod>]
-    member __.TestChainingCachedConcurrency() =
-        let testTask x =
+[<Test>]
+let ``strict cached pair`` () =
+    {   Task = fun () ->
             plan {
-                let! a = send (x + "1")
-                let! b = send (x + "2")
-                let! c = send (x + "3")
-                return a + b + c
+                let! q1 = send "q"
+                let! q2 = send "q"
+                return q1 + q2
             }
-        {   Task = fun () ->
-                plan {
-                    let! x1, x2, x3 =
-                        testTask "x", testTask "x", testTask "x"
-                    return x1 + " " + x2 + " " + x3
-                }
-            Batches =
-                [   [ "x1" ]
-                    [ "x2" ]
-                    [ "x3" ]
-                ]
-            Result = Good "x1x2x3 x1x2x3 x1x2x3"
-        } |> test
+        Batches =
+            [   [ "q" ]
+            ]
+        Result = Good "qq"
+    } |> test
 
-    [<TestMethod>]
-    member __.TestStillValid() =
-        {   Task = fun () ->
-                plan {
-                    let! q1 = send "q"
-                    let! q2 = send "q"
-                    let! m = send "x"
-                    let! q3 = send "q"
-                    return q1 + q2 + m + q3
-                }
-            Batches =
-                [   [ "q" ]
-                    [ "x" ]
-                ]
-            Result = Good "qqxq"
-        } |> test
+[<Test>]
+let ``concurrent cached pair`` () =
+    {   Task = fun () ->
+            plan {
+                let! q1, q2 = send "q", send "q"
+                return q1 + q2
+            }
+        Batches =
+            [   [ "q" ]
+            ]
+        Result = Good "qq"
+    } |> test
 
-    [<TestMethod>]
-    member __.TestInvalidation() =
-        {   Task = fun () ->
-                plan {
-                    let! q1 = send "q"
-                    let! q2 = send "q"
-                    let! m = mutate "x"
-                    let! q3 = send "q"
-                    let! q4 = send "q"
-                    return q1 + q2 + m + q3 + q4
-                }
-            Batches =
-                [   [ "q" ]
-                    [ "x" ]
-                    [ "q" ]
-                ]
-            Result = Good "qqxqq"
-        } |> test
+[<Test>]
+let ``chaining cached concurrency`` () =
+    let testTask x =
+        plan {
+            let! a = send (x + "1")
+            let! b = send (x + "2")
+            let! c = send (x + "3")
+            return a + b + c
+        }
+    {   Task = fun () ->
+            plan {
+                let! x1, x2, x3 =
+                    testTask "x", testTask "x", testTask "x"
+                return x1 + " " + x2 + " " + x3
+            }
+        Batches =
+            [   [ "x1" ]
+                [ "x2" ]
+                [ "x3" ]
+            ]
+        Result = Good "x1x2x3 x1x2x3 x1x2x3"
+    } |> test
 
-    [<TestMethod>]
-    member __.TestDeferredExecution() =
-        let px =
+[<Test>]
+let ``still valid after other`` () =
+    {   Task = fun () ->
+            plan {
+                let! q1 = send "q"
+                let! q2 = send "q"
+                let! m = send "x"
+                let! q3 = send "q"
+                return q1 + q2 + m + q3
+            }
+        Batches =
+            [   [ "q" ]
+                [ "x" ]
+            ]
+        Result = Good "qqxq"
+    } |> test
+
+[<Test>]
+let ``invalidation invalidates`` () =
+    {   Task = fun () ->
+            plan {
+                let! q1 = send "q"
+                let! q2 = send "q"
+                let! m = mutate "x"
+                let! q3 = send "q"
+                let! q4 = send "q"
+                return q1 + q2 + m + q3 + q4
+            }
+        Batches =
+            [   [ "q" ]
+                [ "x" ]
+                [ "q" ]
+            ]
+        Result = Good "qqxqq"
+    } |> test
+
+[<Test>]
+let ``deferred execution`` () =
+    let px =
+        plan {
+            let! q = send "q"
+            let! x = send "x"
+            return x
+        }
+    let py = send "y"
+    {   Task = fun () ->
             plan {
                 let! q = send "q"
-                let! x = send "x"
-                return x
+                // when px and py are batched together, at first there is a step with both
+                // q (from px) and y (from py) pending.
+                // q will be pulled from the cache, but rather than just executing y, we should
+                // defer y and advance px so we can batch x and y together.
+                let! x, y = px, py
+                let! z = send "z"
+                return q + x + y + z
             }
-        let py = send "y"
-        {   Task = fun () ->
-                plan {
-                    let! q = send "q"
-                    // when px and py are batched together, at first there is a step with both
-                    // q (from px) and y (from py) pending.
-                    // q will be pulled from the cache, but rather than just executing y, we should
-                    // defer y and advance px so we can batch x and y together.
-                    let! x, y = px, py
-                    let! z = send "z"
-                    return q + x + y + z
-                }
-            Batches =
-                [   [ "q" ]
-                    [ "x"; "y" ] // x and y batched together
-                    [ "z" ]
-                ]
-            Result = Good "qxyz"
-        } |> test
+        Batches =
+            [   [ "q" ]
+                [ "x"; "y" ] // x and y batched together
+                [ "z" ]
+            ]
+        Result = Good "qxyz"
+    } |> test
