dd6bee299eb0b177150da58681c8bf839e0c278a
Robert Peele <robert.s.peele@gmail.com>
2016-11-09 00:47:49 -0500

    Use FsUnit in Rezoom.IPGeo.Test.

--- Rezoom.IPGeo.Test/TestGeo.fs
+++ Rezoom.IPGeo.Test/TestGeo.fs
@@ -1,60 +1,59 @@
-﻿namespace Rezoom.IPGeo.Test
+﻿module Rezoom.IPGeo.Test.TestGeo
 open Rezoom
 open Rezoom.IPGeo
-open Microsoft.VisualStudio.TestTools.UnitTesting
+open NUnit.Framework
+open FsUnit
 
-[<TestClass>]
-type TestGeo() =
-    static let googleDNS = "8.8.8.8"
-    static let googleDNS2 = "8.8.4.4"
-    static let openDNS = "208.67.220.220"
-    static let fb = "2a03:2880:2110:df07:face:b00c::1"
+let googleDNS = "8.8.8.8"
+let googleDNS2 = "8.8.4.4"
+let openDNS = "208.67.220.220"
+let fb = "2a03:2880:2110:df07:face:b00c::1"
 
-    static let googleDNSISP = "Google"
-    static let googleDNS2ISP = "Level 3 Communications"
-    static let openDNSISP = "OpenDNS, LLC"
-    static let fbISP = "Facebook"
+let googleDNSISP = "Google"
+let googleDNS2ISP = "Level 3 Communications"
+let openDNSISP = "OpenDNS, LLC"
+let fbISP = "Facebook"
 
-    [<TestMethod>]
-    member __.TestBatches() =
-        {   Task =
-                plan {
-                    let! g1, g2 = Geo.Locate(googleDNS), Geo.Locate(googleDNS2)
-                    let! o = Geo.Locate(openDNS)
-                    return g1.Isp, g2.Isp, o.Isp
-                }
-            Batches =
-                [   [googleDNS; googleDNS2]
-                    [openDNS]
-                ]
-            ExpectedResult = Value (googleDNSISP, googleDNS2ISP, openDNSISP)
-        } |> test
+[<Test>]
+let ``batches`` () =
+    {   Task =
+            plan {
+                let! g1, g2 = Geo.Locate(googleDNS), Geo.Locate(googleDNS2)
+                let! o = Geo.Locate(openDNS)
+                return g1.Isp, g2.Isp, o.Isp
+            }
+        Batches =
+            [   [googleDNS; googleDNS2]
+                [openDNS]
+            ]
+        ExpectedResult = Value (googleDNSISP, googleDNS2ISP, openDNSISP)
+    } |> test
 
-    [<TestMethod>]
-    member __.TestCaching() =
-        {   Task =
-                plan {
-                    let! g1 = Geo.Locate(googleDNS)
-                    let! g2, o = Geo.Locate(googleDNS), Geo.Locate(openDNS)
-                    return g1.Isp, g2.Isp, o.Isp
-                }
-            Batches =
-                [   [googleDNS]
-                    [] // empty batch because we defer openDNS after getting google from the cache
-                    [openDNS]
-                ]
-            ExpectedResult = Value (googleDNSISP, googleDNSISP, openDNSISP)
-        } |> test
+[<Test>]
+let ``caching`` () =
+    {   Task =
+            plan {
+                let! g1 = Geo.Locate(googleDNS)
+                let! g2, o = Geo.Locate(googleDNS), Geo.Locate(openDNS)
+                return g1.Isp, g2.Isp, o.Isp
+            }
+        Batches =
+            [   [googleDNS]
+                [] // empty batch because we defer openDNS after getting google from the cache
+                [openDNS]
+            ]
+        ExpectedResult = Value (googleDNSISP, googleDNSISP, openDNSISP)
+    } |> test
 
-    [<TestMethod>]
-    member __.TestDedup() =
-        {   Task =
-                plan {
-                    let! g1, fb1, fb2 = Geo.Locate(googleDNS), Geo.Locate(fb), Geo.Locate(fb)
-                    return g1.Isp, fb1.Isp, fb2.Isp
-                }
-            Batches =
-                [   [googleDNS; fb] // note that we don't request fb twice
-                ]
-            ExpectedResult = Value (googleDNSISP, fbISP, fbISP)
-        } |> test
+[<Test>]
+let ``dedup`` () =
+    {   Task =
+            plan {
+                let! g1, fb1, fb2 = Geo.Locate(googleDNS), Geo.Locate(fb), Geo.Locate(fb)
+                return g1.Isp, fb1.Isp, fb2.Isp
+            }
+        Batches =
+            [   [googleDNS; fb] // note that we don't request fb twice
+            ]
+        ExpectedResult = Value (googleDNSISP, fbISP, fbISP)
+    } |> test
