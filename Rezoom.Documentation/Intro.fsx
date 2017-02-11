d2e519755aba42f05faa3a9fa13f6fefda731ac4
Robert Peele <robert.s.peele@gmail.com>
2017-02-11 18:15:53 -0500

    Stub for documentation project.

---
+++ Rezoom.Documentation/Intro.fsx
@@ -0,0 +1,21 @@
+﻿(*** hide ***)
+
+#r "../Rezoom.SQL.Provider/bin/Debug/Rezoom.dll"
+#r "../Rezoom.SQL.Provider/bin/Debug/LicenseToCIL.dll"
+#r "../Rezoom.SQL.Provider/bin/Debug/Rezoom.SQL.dll"
+#r "../Rezoom.SQL.Provider/bin/Debug/Rezoom.SQL.Mapping.dll"
+#r "../Rezoom.SQL.Provider/bin/Debug/Rezoom.SQL.Provider.dll"
+#nowarn "193"
+open System
+open Rezoom
+open Rezoom.SQL
+open Rezoom.SQL.Provider
+
+(**
+
+# Rezoom: a resumption monad for .NET data access.
+
+Rezoom is a library intended to help you code an API for manipulating your application's data.
+An API that doesn't fall down when you try to compose its functions.
+
+*)
