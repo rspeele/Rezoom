01b66e110834ec8aecece27fdfe8f1c9edf70448
Robert Peele <humbobst@gmail.com>
2026-05-17 02:05:32 -0400

    Rename more idiomatically.

--- src/Rezoom/Shims.fs
+++ src/Rezoom/Shims.fs
@@ -2,6 +2,10 @@
 // `AsyncMethodBuilderAttribute` was added to the BCL in .NET Standard 2.1 / .NET 6.
 // Defining it here lets us target netstandard2.0 without the consumer needing
 // to reference System.Threading.Tasks.Extensions or similar.
+// Roslyn resolves the attribute by name so this still works for the netstandard2.0
+// consumer as long as their compiler version is up to date.
+// On net8.0+ this file compiles to nothing and the BCL's
+// real attribute is used; the two never coexist in the same assembly.
 namespace System.Runtime.CompilerServices
 
 open System
@@ -12,11 +16,11 @@ open System
       ||| AttributeTargets.Enum ||| AttributeTargets.Method
     , Inherited = false
     , AllowMultiple = false)>]
-type internal AsyncMethodBuilderAttribute(builderType : Type) =
+type AsyncMethodBuilderAttribute(builderType : Type) =
     inherit Attribute()
     member _.BuilderType = builderType
 #else
 namespace Rezoom.Internal
 // Placeholder so this file produces something on non-netstandard2.0 targets.
-module internal Polyfills = ()
+module internal Shims = ()
 #endif
